3712d46b56ac9fc81651322f3e2f27b77e5147f9
duyongping <786022775@qq.com>
2019-01-11 17:44:22 +0800

    主页去掉layui框架，采用全原生-jquery

--- src/main/resources/static/common/commonSecond.js
+++ src/main/resources/static/common/commonSecond.js
@@ -1,9 +1,5 @@
 // 二级common
 document.write('<script src="../../../static/_plugin/jquery.min.js" type="text/javascript"></script>');
-document.write('<script src="../../../static/_plugin/jquery-ui.js" type="text/javascript"></script>');
-document.write('<link href="../../../static/_plugin/jquery-ui.css" rel="stylesheet" type="text/css">');
-document.write('<link href="../../../static/_plugin/jquery-ui.structure.css" rel="stylesheet" type="text/css">')
-document.write('<link href="../../../static/_plugin/jquery-ui.theme.css" rel="stylesheet" type="text/css">')
 
 // 公共的一些样式
 document.write('<link href="../../../static/css/common/common.css" rel="stylesheet" type="text/css">')
